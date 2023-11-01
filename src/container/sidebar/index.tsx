f755e4a93229aa81aa21067bed4a72ba8845fe5b
JorgeWendell <141657061+JorgeWendell@users.noreply.github.com>
2023-11-01 14:37:27 -0300

    estilizando a sidebar

--- src/container/sidebar/index.tsx
+++ src/container/sidebar/index.tsx
@@ -1,10 +1,22 @@
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/jorgewendell.png" />
-    <Titulo fontSize={20}>Jorge Wendell</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Jorge Wendell</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        jorgewendell
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Programador Junior
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
