139c57483bac31e40447ab0e7bc69bd52d3f9f6e
JorgeWendell <141657061+JorgeWendell@users.noreply.github.com>
2023-11-01 14:05:27 -0300

    Criando a sesão projetos e componentes

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import styled from 'styled-components'
+import Paragrafo from '../Paragrafo'
+import Titulo from '../Titulo'
+
+import { Card, LinkBotao } from './styles'
+
+const Projeto = () => (
+  <Card>
+    <Titulo>Lista de Tarefas</Titulo>
+    <Paragrafo tipo="secundario">Lista de tarefas feita com VueJS</Paragrafo>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+
+export default Projeto
